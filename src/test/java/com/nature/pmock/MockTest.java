e0b47ee7944f1619a852d7b9d8ca50acfbd74d85
ppicinic <PhilPicinic@gmail.com>
2013-08-12 11:55:23 -0400

    fixed MockTest to use Log

--- src/test/java/com/nature/pmock/MockTest.java
+++ src/test/java/com/nature/pmock/MockTest.java
@@ -2,50 +2,55 @@ package com.nature.pmock;
 
 import static org.junit.Assert.fail;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 
-public class MockTest{
+@PowerMockIgnore("org.apache.*")
+public class MockTest {
+
+    private Logger log = Logger.getLogger(MockTest.class);
 
     @Test
-    public void InvalidDate(){
+    public void InvalidDate() {
         try {
             // TODO: Use PowerMock to expect getThen() and return an invalid date (a date from over a week ago)
         } catch (RuntimeException re) {
-            
-        } catch (Exception e){
-            System.out.println(e);
+
+        } catch (Exception e) {
+            log.fatal(e);
             fail();
         }
     }
-    
+
     @Test
-    public void InvokeConstructor(){
-        try{
+    public void InvokeConstructor() {
+        try {
             // TODO: Leverage Whitebox to invoke constructor
-        } catch(Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            log.fatal(e);
             fail();
         }
     }
-    
+
     @Test
-    public void ExpectExists(){
-        try{
+    public void ExpectExists() {
+        try {
             // TODO: Use PowerMock to expect exists() and return false to force the constructor to run.
-        } catch(Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            log.fatal(e);
             fail();
         }
     }
-    
+
     @Test
-    public void SetState(){
-        try{
+    public void SetState() {
+        try {
             // TODO: Use Whitebox to set date in Then to null
-        } catch(Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            log.fatal(e);
             fail();
         }
     }
-    
+
 }
